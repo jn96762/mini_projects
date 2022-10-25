99cd43909b41f8a602eb640bf38c675c43084950
jn96762 <89213421+jn96762@users.noreply.github.com>
2022-10-25 14:36:12 -0500

    Delete FAQ_boxes directory

--- FAQ_boxes/script.js
+++
@@ -1,9 +0,0 @@
-const buttons = document.querySelectorAll('.faq-toggle');
-
-buttons.forEach(button => {
-    const box = button.parentNode;
-    button.addEventListener('click', () => {
-        box.classList.toggle('active');
-    })
-})
-
